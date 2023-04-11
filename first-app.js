b299b596b1b6469d419ffd93ff01a926c01093bf
Olivia Xu <110135215+oliviaxu0528@users.noreply.github.com>
2023-04-11 11:44:47 -0700

    input response

--- first-app.js
+++ first-app.js
@@ -1,4 +1,8 @@
 console.log('Hello from Node.js');
 
+// const fs = require('fs');
+// fs.writeFileSync('hello.txt','Hello from Node.js');
+
 const fs = require('fs');
-fs.writeFileSync('hello.txt','Hello from Node.js');
+
+fs.writeFileSync('hello2.txt','Hello from Node.js');
