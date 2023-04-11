33e69f51e9abf961c575fd4b340e823706593da2
Olivia Xu <110135215+oliviaxu0528@users.noreply.github.com>
2023-04-11 15:08:04 -0700

    buffer

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
   const url = req.url;
+  const method = req.method;
   if (url == '/'){
     res.setHeader('Content-Type','text/html');
     res.write('<html');
@@ -10,7 +12,28 @@ const server = http.createServer((req,res)=>{
     res.write('</html>');
     return res.end();
   }
+  if (url == "/message"&& method ==="POST"){
+    const body = [];
+    req.on('data',(chuck)=>{
+      console.log("chuck: ", chuck);
+      body.push(chuck);
+    });
+    req.on('end',()=>{
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message2.txt', message);
+    })
+    res.statusCode = 302;
+    res.setHeader('Location','/');
+    return res.end();
 
+  }
+  res.setHeader('Content-Type','text/html');
+  res.write('<html');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js!</h1></body>')
+  res.write('</html');
+  res.end();
 })
 
 server.listen(3000);
